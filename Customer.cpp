c962faf1cc3b4ee29ae72d789d5822bf31c23149
Ellen-Dennison <87267126+Ellen-Dennison@users.noreply.github.com>
2025-08-31 20:22:03 +0200

    main changes

--- Customer.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Observer.h"
-#include "Customer.h"
-
-
-void Customer::update(std::string message)
-{ std::cout << "CUSTOMER THERE HAS BEEN A CHANGE: " << message << std::endl; }
--- Website.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "Observer.h"
-#include "Website.h"
-
-
-void Website::update(std::string message)
-{ std::cout << "WEBSITE USER THERE HAS BEEN A CHANGE: " << message << std::endl; }
